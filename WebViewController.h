91271a6da5af91f2c70e9a45feec825c34cb86ce
Olivia <oliviantaylor@gmail.com>
2016-08-10 16:43:50 -0400

    added gitignore

---
+++ WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  NavCtrl
+//
+//  Created by Olivia Taylor on 8/10/16.
+//  Copyright © 2016 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
